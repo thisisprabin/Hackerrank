07659c36c46755ea4c396177adcbef65e3b5417c
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-01-30 20:00:54 +0530

    Add files via upload

---
+++ Data-Structures/Stacks/Maximum-Element/MaximumElement.java
@@ -0,0 +1,31 @@
+package p1;
+
+import java.util.*;
+
+public class MaximumElement {
+
+	public static void main(String... args) {
+		Scanner in = new Scanner(System.in);
+		LinkedList<Integer> head = new LinkedList<Integer>();
+		
+		int test = in.nextInt();
+		
+		for(int i=0; i<test; i++){
+			int ch = in.nextInt();
+			
+			switch(ch) {
+				case 1: int data = in.nextInt();
+						head.add(data);
+				break;
+				
+				case 2: head.removeLast();
+				break;
+				
+				case 3: int max = Collections.max(head);
+						System.out.println(max);
+				break;		
+			}
+		}
+		in.close();
+	}
+}
